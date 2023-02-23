976aed02357225a4580adfb874b82c3c066c04a3
albertomerango <alberto.marengo@coeuraj.com>
2023-02-22 17:02:22 -0800

    updated packages in .toml

--- llmtosql/dataloader.py
+++ llmtosql/dataloader.py
@@ -1,11 +1,30 @@
-import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 
 
-class Loader(DataLoader):
-    pass
-    # def __init__(self, train_set, batch_size, shuffle=False, sampler=None):
-    #     pass
-    #
-    # def __getitem__(self, item):
-    #     pass
+class WikiSQLDataset(Dataset):
+    def __init__(self, path):
+        with open(path) as f:
+            self.data = [eval(line) for line in f]
+            self.maxcondsLength = max(len(x['sql']['conds']) for x in self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        table = self.data[item]['table_id']
+        input = self.data[item]['question']
+        sel = self.data[item]['sql']['sel']
+        agg = self.data[item]['sql']['agg']
+        conds = [{'cond_1': int(cond[0]), 'cond_2': int(cond[1]), 'cond_3': str(cond[2])} for cond in self.data[item]['sql']['conds']]
+        if len(conds) != self.maxcondsLength:
+            list_extension = [{'cond_1': np.NaN, 'cond_2': np.NaN, 'cond_3': ''}]*(self.maxcondsLength-len(conds))
+            conds.extend(list_extension)
+        return {
+            'table_id': str(table),
+            'input': str(input),
+            'labels': {
+                'sel': int(sel),
+                'agg': int(agg),
+                'conds': conds
+            }
+        }
--- llmtosql/model.py
+++ llmtosql/model.py
@@ -1,24 +1,72 @@
+from transformers import AutoModel, AutoTokenizer
 import torch
-from torch.nn import Module
-from torch.nn import functional as F
 from torch import nn
+import structlog
+
+logger = structlog.get_logger('__name__')
 
 
-class MLModel(Module):
-    def __init__(self):
-        super(MLModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+class WikiSQLModel(nn.Module):
+    def __init__(self, base_model_type, N_lat=None):
+        super().__init__()
+        if not base_model_type:
+            logger.error(f'{type(base_model_type)}  not valid')
+            raise TypeError(f'{type(base_model_type)}  not valid')
+        if 'gpt' in base_model_type:
+            self.base_model = 'gpt'
+        elif 'bert' in base_model_type:
+            self.base_model = 'bert'
+        else:
+            logger.error(f'Model type not valid - {base_model_type}')
+            raise TypeError(f'Model type not valid - {base_model_type}')
+        self.tokenizer = AutoTokenizer.from_pretrained(base_model_type)
+        self.model = AutoModel.from_pretrained(base_model_type)
+        self.seq_length = self.model.config.max_position_embeddings
+        if not N_lat:
+            self.hidden_dim = self.model.config.hidden_size
+        self.cross_att = nn.MultiheadAttention(self.hidden_dim, 8)
+        self.batch_norm = nn.BatchNorm1d(self.seq_length)
+        self.out = nn.Linear(self.hidden_dim, 1)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    def forward(self, data):
+        text_imp, columns_imp = data
+        if self.base_model == 'gpt':
+            self.tokenizer.add_special_tokens({'pad_token': '<pad>'})
+            self.model.resize_token_embeddings(len(self.tokenizer))
+            columns_imp = [self.tokenizer.eos_token + ' ' + col_txt for col_txt in columns_imp]
+            text_imp = [self.tokenizer.eos_token + ' ' + col_txt for col_txt in text_imp]
+        text_tokenized = self.tokenizer(text_imp, padding='max_length', return_tensors='pt')
+        columns_tokenized = self.tokenizer(columns_imp, padding='max_length', return_tensors='pt')
+        with torch.no_grad():
+            text_outputs = self.model(**text_tokenized)
+            columns_outputs = self.model(**columns_tokenized)
+        text_last_hs = text_outputs.last_hidden_state
+        columns_last_hs = columns_outputs.last_hidden_state
+        attn_output, _ = self.cross_att(text_last_hs, text_last_hs, columns_last_hs)
+        cross_attention_norm = self.batch_norm(attn_output)
+        cross_layer_out = torch.add(columns_last_hs, cross_attention_norm)
+        ret = self.out(cross_layer_out).squeeze()
+        return self.compose_outputs(columns_tokenized, ret)
+
+    def compose_outputs(self, col_vector, final_vector):
+        comma = self.tokenizer.convert_tokens_to_ids(',')
+        comma_idx = (col_vector['input_ids'] == comma).nonzero(as_tuple=True)
+        dim_1 = torch.max(torch.unique(comma_idx[0], return_counts=True)[1]).item() + 1
+        dim_0 = len(col_vector['input_ids'])
+        out = torch.zeros([dim_0, dim_1], dtype=torch.float32)
+        slice_start = 0
+        current_idx = 0
+        y = 0
+        for idx in range(comma_idx[0].shape[0]):
+            slice_end = comma_idx[1][idx].item()
+            x = comma_idx[0][idx].item()
+            if x != current_idx:
+                out[x-1][y] = final_vector[x-1][slice_start:].sum()
+                y = 0
+                slice_start = 0
+                current_idx = x
+            out[x][y] = final_vector[x][slice_start:slice_end].sum()
+            slice_start = slice_end + 1
+            y += 1
+        out[x][y] = final_vector[x][slice_start:].sum()
+        return out
