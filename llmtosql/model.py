e750a79160fca0dfb481e148e4633d9fdb380792
albertomerango <alberto.marengo@coeuraj.com>
2023-03-09 07:39:06 -0800

    New architecture - select only

--- llmtosql/model.py
+++ llmtosql/model.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 from torch.nn.parameter import Parameter
 import structlog
-import os
+from .utils.modules.select import WikiSQLSelect
 
 logger = structlog.get_logger('__name__')
 
@@ -11,9 +11,27 @@ logger = structlog.get_logger('__name__')
 class WikiSQLModel(nn.Module):
     def __init__(self):
         super().__init__()
-
-
-
+        self.tokenizer = AutoTokenizer.from_pretrained(self.base_model_type)
+        self.model = AutoModel.from_pretrained(self.base_model_type)
+        self.seq_length = self.model.config.max_position_embeddings
+        self.sel_layer = WikiSQLSelect()
 
     def forward(self, data):
-        pass
+        sel_out = self.sel_layer(data)
+        return sel_out
+
+    def tokenize(self, data):
+        text_imp, columns_imp = data
+        if isinstance(text_imp, str) and isinstance(columns_imp, str):
+            text_imp, columns_imp = [text_imp], [columns_imp]
+        if self.col_drop:
+            columns_imp = self.reduce_col_name(columns_imp)
+        text_imp = [text + ' ' + columns for text, columns in zip(text_imp, columns_imp)]
+        if self.base_model == 'gpt':
+            self.tokenizer.add_special_tokens({'pad_token': '<pad>'})
+            self.model.resize_token_embeddings(len(self.tokenizer))
+            columns_imp = [self.tokenizer.eos_token + ' ' + col_txt for col_txt in columns_imp]
+            text_imp = [self.tokenizer.eos_token + ' ' + col_txt for col_txt in text_imp]
+        text_tokenized = self.tokenizer(text_imp, padding='max_length', return_tensors='pt')
+        columns_tokenized = self.tokenizer(columns_imp, padding='max_length', return_tensors='pt')
+        return text_tokenized, columns_tokenized
--- llmtosql/utils/modules/base_model.py
+++ llmtosql/utils/modules/base_model.py
@@ -1,4 +1,3 @@
-from transformers import AutoModel, AutoTokenizer
 import torch
 from torch import nn
 from torch.nn.parameter import Parameter
@@ -12,6 +11,7 @@ class WikiSQLBase(nn.Module):
     def __init__(self, base_model_type, N_lat=None, attention_type='cross', col_drop=False, local_model_type=None):
         super().__init__()
         self.parameter = Parameter(torch.empty((1, 1)))
+        self.base_model_type = base_model_type
         self.N_lat = N_lat
         self.attention_type = attention_type
         self.col_drop = col_drop
@@ -35,9 +35,6 @@ class WikiSQLBase(nn.Module):
             else:
                 logger.error(f'Model type not valid - {base_model_type}')
                 raise TypeError(f'Model type not valid - {base_model_type}')
-        self.tokenizer = AutoTokenizer.from_pretrained(base_model_type)
-        self.model = AutoModel.from_pretrained(base_model_type)
-        self.seq_length = self.model.config.max_position_embeddings
         if not self.N_lat:
             self.hidden_dim = self.model.config.hidden_size
         if self.attention_type == 'cross':
@@ -48,22 +45,6 @@ class WikiSQLBase(nn.Module):
         else:
             logger.error(f'{type(self.attention_type)}  not valid')
             raise TypeError(f'{type(self.attention_type)}  not valid')
-
-    def tokenize(self, data):
-        text_imp, columns_imp = data
-        if isinstance(text_imp, str) and isinstance(columns_imp, str):
-            text_imp, columns_imp = [text_imp], [columns_imp]
-        if self.col_drop:
-            columns_imp = self.reduce_col_name(columns_imp)
-        text_imp = [text + ' ' + columns for text, columns in zip(text_imp, columns_imp)]
-        if self.base_model == 'gpt':
-            self.tokenizer.add_special_tokens({'pad_token': '<pad>'})
-            self.model.resize_token_embeddings(len(self.tokenizer))
-            columns_imp = [self.tokenizer.eos_token + ' ' + col_txt for col_txt in columns_imp]
-            text_imp = [self.tokenizer.eos_token + ' ' + col_txt for col_txt in text_imp]
-        text_tokenized = self.tokenizer(text_imp, padding='max_length', return_tensors='pt')
-        columns_tokenized = self.tokenizer(columns_imp, padding='max_length', return_tensors='pt')
-        return text_tokenized, columns_tokenized
 
     def forward(self, data):
         pass
