a20865b410e9fe79d63aa483e1ca0d0ec25168f4
albertomerango <alberto.marengo@coeuraj.com>
2023-03-15 15:25:41 -0700

    Worked on plot history after 2 head model working

--- llmtosql/utils/utils.py
+++ llmtosql/utils/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 from collections import OrderedDict
 from matplotlib import pyplot as plt
 import torch
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 def load_history(file_dir):
@@ -66,3 +68,35 @@ def plot_history(history: dict) -> None:
         plt.legend()
     plt.tight_layout()
     plt.show()
+
+
+def plot_history_plotly(history: dict) -> None:
+    x = history['epochs']
+    train_loss = history['train_loss']
+    val_loss = history['val_loss']
+    if history['metric_type'] != None:
+        train_metrics = list(map(list, zip(*history['train_metric'])))
+        val_metrics = list(map(list, zip(*history['val_metric'])))
+        fig = make_subplots(
+            rows=2, cols=2,
+            specs=[[{"colspan": 2}, None],
+                   [{}, {}]],
+            subplot_titles=("Training Loss vs. Validation Loss",
+                            f"{history['metric_type']} - Training vs. Validation",
+                            f"{history['metric_type']} - Training vs. Validation"))
+        fig.add_trace(go.Scatter(x=x, y=train_loss, name='Train Loss',
+                                 line = dict(color='black', width=3)), row=1, col=1)
+        fig.add_trace(go.Scatter(x=x, y=val_loss, name='Validation Loss',
+                                 line=dict(color='black', width=3, dash='dash')), row=1, col=1)
+        fig.update_xaxes(title_text='Epochs', row=1, col=1)
+        fig.update_yaxes(title_text='Loss', row=1, col=1)
+        for idx, (train_metric, val_metric) in enumerate(zip(train_metrics, val_metrics), start=1):
+            fig.add_trace(go.Scatter(x=x, y=train_metric, name=f"Train {history['metric_type']}",
+                                     line=dict(color='blue', width=3)), row=2, col=idx)
+            fig.add_trace(go.Scatter(x=x, y=val_metric, name=f"Validation {history['metric_type']}",
+                                     line=dict(color='blue', width=3, dash='dash')), row=2, col=idx)
+            fig.update_xaxes(title_text='Epochs', row=2, col=idx)
+            fig.update_yaxes(title_text='Accuracy', row=2, col=idx)
+        fig.update_layout(showlegend=True, title_text="Model Training Results", hovermode='x unified')
+        fig.update_layout(legend=dict(y=0.5, font_size=16), width=1200, height=800)
+        fig.show()
