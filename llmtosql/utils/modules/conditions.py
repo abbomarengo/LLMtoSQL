133fcd8d12ad791136953a6a2e1fd9a1fc8ccfc5
albertomerango <alberto.marengo@coeuraj.com>
2023-03-19 20:25:07 -0700

    3 head model - worked on text condition:testing:latest

--- llmtosql/utils/modules/conditions.py
+++ llmtosql/utils/modules/conditions.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 from torch.nn import Softmax
-from torch.nn.parameter import Parameter
 import structlog
 
 from .select import WikiSQLSelect
@@ -13,7 +12,6 @@ logger = structlog.get_logger('__name__')
 class WikiSQLConditions(nn.Module):
     def __init__(self, tokenizer, hidden_dim, seq_len, vocab_size, attention_type):
         super().__init__()
-        # self.parameter = Parameter(torch.empty((1, 1)))
         self.tokenizer = tokenizer
         self.hidden_dim = hidden_dim
         self.seq_len = seq_len
@@ -24,12 +22,10 @@ class WikiSQLConditions(nn.Module):
             # Step 1 - condition number
             self.cond_num_layer = WikiSQLSAgg(self.hidden_dim, 10, attention_type)
             # Step 4 - Condition text
-            # self.q = nn.Linear(1, self.hidden_dim)
-            self.cross_att = nn.MultiheadAttention(self.hidden_dim, 8, batch_first=True)
-            self.batch_norm = nn.BatchNorm1d(self.hidden_dim)
+            self.cond_text_layer = WikiSQLSAgg(self.hidden_dim, self.hidden_dim, attention_type)
             self.ff = nn.Sequential(nn.Linear(self.hidden_dim, self.hidden_dim),
-                                         nn.Tanh(), nn.Linear(self.hidden_dim, self.hidden_dim))
-            self.out = nn.Linear(self.hidden_dim, self.vocab_size)
+                                    nn.Tanh(), nn.Linear(self.hidden_dim, self.hidden_dim))
+            self.text_out = nn.Linear(self.hidden_dim, self.vocab_size)
         elif self.attention_type == 'sqlnet':
             logger.error(f'SQLNET not implemented for the Condition Head')
             raise TypeError(f'SQLNET not implemented for the Condition Head')
@@ -47,22 +43,12 @@ class WikiSQLConditions(nn.Module):
         dim_0 = text_last_hs.shape[0]
         dim_1 = self.seq_len
         dim_2 = num_conditions
-        # device_n = self.parameter.get_device()
-        # if self.parameter.is_cuda:
-        #     device = 'cuda:' + str(device_n) if device_n != -1 else 'cpu'
-        # else:
-        #     device = 'mps:' + str(device_n) if device_n != -1 else 'cpu'
-        # mask_id = self.tokenizer.convert_tokens_to_ids('[MASK]')
-        # dec_inp = torch.ones([dim_0, dim_1], dtype=torch.float32, device=device)*mask_id
-        # query = self.q(dec_inp.unsqueeze(dim=-1))
-        attn_output, _ = self.cross_att(columns_last_hs, text_last_hs, text_last_hs)
-        cross_attention_add = torch.add(columns_last_hs, attn_output)
-        cross_attention_norm = self.batch_norm(torch.transpose(cross_attention_add, 1, 2))
+        cross_attention_norm = self.cond_text_layer(data)
         cross_transpose = torch.transpose(cross_attention_norm, 1, 2)
         concat = torch.cat([cross_transpose]*dim_2, dim=-1)
         reshaped_in = concat.view(dim_0, dim_1, dim_2, self.hidden_dim)
         feed_forward = self.ff(reshaped_in)
-        cond_text_out = self.out(feed_forward)
+        cond_text_out = self.text_out(feed_forward)
 
         return cond_text_out
 
